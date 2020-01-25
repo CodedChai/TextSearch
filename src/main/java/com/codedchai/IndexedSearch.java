8ed084e93b778a4e1554dbb5cbd909d83cfc3a8e
Connor Hasting <guardianx515@gmail.com>
2020-01-25 14:46:29 -0600

    Added some basic suffix stemming

--- src/main/java/com/codedchai/IndexedSearch.java
+++ src/main/java/com/codedchai/IndexedSearch.java
@@ -7,7 +7,7 @@ public class IndexedSearch extends Search {
 	protected Map < String, Map < String, Set < Integer > > > documentWordIndexMap = null;
 
 	protected static final String SPLIT_WHITESPACE = "\\s+";
-
+	List<String> suffixes = Arrays.asList( "ed", "ing", "ly", "s" );
 	protected Set<String> stopwords = initializeStopwords();
 
 	@Override
@@ -31,7 +31,7 @@ public class IndexedSearch extends Search {
 					Set<Integer> tempValidIndices = new HashSet <>(  );
 
 					for(Integer currentValidIndex : currentIndices){
-						if(nextIndices.contains( currentValidIndex + 1 )){
+						if(nextIndices != null && nextIndices.contains( currentValidIndex + 1 )){
 							tempValidIndices.add( currentValidIndex + 1 );
 						}
 					}
@@ -78,7 +78,7 @@ public class IndexedSearch extends Search {
 		text = lowerCase( text );
 		text = stripSpecialCharacters(text);
 		text = stopwordRemoval( text );
-
+		text = stem(text);
 		return text.strip();
 	}
 
@@ -130,6 +130,24 @@ public class IndexedSearch extends Search {
 		stopwordsSet.addAll( Arrays.asList( "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours	", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ) );
 
 		return stopwordsSet;
+	}
+
+	public String stem(String text){
+		StringBuilder stringBuilder = new StringBuilder(  );
+
+		for(String word : text.split( SPLIT_WHITESPACE )){
+
+			String stemmableSuffix = suffixes.stream().filter( suffix -> word.endsWith( suffix ) && !word.equals( suffix ) ).findFirst().orElse( "" );
+
+			if(!stemmableSuffix.isBlank()){
+				stringBuilder.append( word, 0, word.length() - stemmableSuffix.length() );
+			} else {
+				stringBuilder.append( word );
+			}
+			stringBuilder.append( " " );
+
+		}
+		return stringBuilder.toString();
 	}
 
 	public static void main( String[] args ) throws Exception {
